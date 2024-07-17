25e508ceed955ecba32c0252dbc2370cea3c130f
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-17 23:18:19 +0100

    updated feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import PromptCard from "@components/PromptCard";
+
+import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="mt-16 prompt_layout">
+    <div className='mt-16 prompt_layout'>
       {data.map((post) => (
         <PromptCard
           key={post._id}
@@ -19,74 +19,75 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
-  const router = useRouter();
-
-  const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const handleTagClick = (tag) => {
-    setSearchText(tag);
-  };
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const res = await fetch("/api/prompt");
-    const data = await res.json();
-    console.log(data);
-    setPosts(data);
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
   };
 
   useEffect(() => {
     fetchPosts();
+  }, []);
 
-    // Listen for route changes to re-fetch posts
-    const handleRouteChange = () => {
-      fetchPosts();
-    };
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
 
-    router.events.on("routeChangeComplete", handleRouteChange);
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
 
-    // Cleanup the event listener on component unmount
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router]);
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
 
-  const filteredPrompts = posts.filter(
-    (post) =>
-      post.prompt.toLowerCase().includes(searchText.toLowerCase()) ||
-      (typeof post.tag === "string" &&
-        post.tag.toLowerCase().includes(searchText.toLowerCase())) ||
-      post.creator.username.toLowerCase().includes(searchText.toLowerCase())
-  );
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
-    <section className="feed">
-      <form onSubmit={handleSubmit} className="relative w-full flex-center">
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
         <input
-          type="text"
-          placeholder="Search for a tag or username"
+          type='text'
+          placeholder='Search for a tag or a username'
           value={searchText}
           onChange={handleSearchChange}
           required
-          className="search_input peer"
+          className='search_input peer'
         />
       </form>
 
-      {/* Display search results if true, else display all prompts */}
+      {/* All Prompts */}
       {searchText ? (
         <PromptCardList
-          data={filteredPrompts}
+          data={searchedResults}
           handleTagClick={handleTagClick}
         />
       ) : (
-        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
       )}
     </section>
   );
