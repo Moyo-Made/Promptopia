1977e912d642913101f1c363f0f0eb4fadf9670b
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-17 21:00:53 +0100

    fix update prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,70 +1,22 @@
-"use client";
+import UpdatePromptClient from "@components/UpdatePromptClient";
+import { Suspense } from "react";
 
-import { Suspense, useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@components/Form";
+const UpdatePromptPage = async ({ searchParams }) => {
+  const promptId = searchParams.id;
 
-const UpdatePrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Missing PromptId!");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
+  const fetchPromptDetails = async (id) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/prompt/${id}`);
+    const data = await response.json();
+    return data;
   };
+
+  const promptData = await fetchPromptDetails(promptId);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
+      <UpdatePromptClient promptData={promptData} />
     </Suspense>
   );
 };
 
-export default UpdatePrompt;
+export default UpdatePromptPage;
