8d964ffc0a05aa221ba36c3bf02524739cabd67b
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-17 23:13:07 +0100

    updated feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import PromptCard from "@components/PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
